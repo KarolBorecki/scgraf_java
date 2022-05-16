c5a86759c040ee2c89524176673407577cde21c7
Karol Borecki <karol.borecki@onet.eu>
2022-05-16 17:49:05 +0200

    Made FIFO resizeable

--- Main.java
+++ Main.java
@@ -8,12 +8,12 @@ public class Main {
         f.push(2);
         System.out.println(f.pop());
         System.out.println(f.pop());
-        System.out.println(f.pop());
-        System.out.println(f.pop());
         f.push(6);
         f.push(7);
         f.push(8);
         f.push(9);
+        System.out.println(f.pop());
+        System.out.println(f.pop());
         f.push(10);
         f.push(11);
     }
--- structures/Fifo.java
+++ structures/Fifo.java
@@ -2,26 +2,30 @@ package structures;
 
 public class Fifo{
     public Object[] queue;
-    public int size;
     public int head = 0;
     public int tail = 0;
 
+    private static final int RESIZE_FACTOR = 2;
+
     public Fifo(int size){
         queue = new Object[size];
-        this.size = size;
     }
 
     public void push(Object o) throws IndexOutOfBoundsException{
-        if(tail >= size)
-            throw new IndexOutOfBoundsException("The queue tail is out of it's bounds!");
-
+        if(tail >= queue.length)
+            resizeQueue(RESIZE_FACTOR);
         queue[tail++] = o;
     }
 
     public Object pop() throws IndexOutOfBoundsException{
-        System.out.println("tail: " + tail + " head: " + head + " size: " + size);
-        if(head >= tail || head >= size)
+        if(head >= tail || head >= queue.length)
             throw new IndexOutOfBoundsException("The queue head is out of it's bounds!");//TODO let logger handle this
         return queue[head++];
+    }
+
+    private void resizeQueue(int resizeFactor){
+        Object[] newQueue = new Object[resizeFactor * queue.length];
+        System.arraycopy(queue, 0, newQueue, 0, queue.length);
+        queue = newQueue;
     }
 }
