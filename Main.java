ffff05092eafce658df34d0eac49b9a27ad5a781
Karol Borecki <karol.borecki@onet.eu>
2022-05-16 19:37:25 +0200

    Implemented Swap function

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import utils.Utils;
+
 public class Main {
     public static void main(String[] args){
-        System.out.print("Hello World");
+        Integer[] w = new Integer[3];
+        w[0] = 1;
+        w[1] = 2;
+        w[2] = 3;
+        Utils.Swap(w, 0, 1);
+        System.out.println(w[0]);
+        System.out.println(w[1]);
     }
 }
---
+++ utils/Utils.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Utils {
+    public static void Swap(Object[] arr, int a, int b){
+        Object c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+    }
+}
