573e6fb0fc91cd68c75672e3299c86c141c311cc
Karol Borecki <karol.borecki@onet.eu>
2022-06-02 21:24:14 +0200

    Moved loading and saving functions to Solver

--- src/main/java/com/scgraf/solver/Solver.java
+++ src/main/java/com/scgraf/solver/Solver.java
@@ -4,6 +4,8 @@ import com.scgraf.UI.elements.buttons.FormattedButton;
 import com.scgraf.algorithms.BFS;
 import com.scgraf.algorithms.Dijkstra;
 import com.scgraf.algorithms.DijkstraDivider;
+import com.scgraf.data_handling.FileReaderG;
+import com.scgraf.data_handling.FileWriterG;
 import com.scgraf.data_structures.graph.Graph;
 import com.scgraf.data_structures.graph.Node;
 import com.scgraf.data_structures.tuples.Size;
@@ -15,8 +17,13 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Popup;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Timestamp;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
@@ -86,11 +93,65 @@ public class Solver {
     }
 
     public void generate(int width, int height, double maxWeight){
-
         Task<Graph> generatingTask = new Task<>() {
             @Override
             public Graph call() {
                 return GraphGenerator.Generate(new Size(width, height), maxWeight);
+            }
+        };
+        generatingTask.setOnRunning( event -> OnSolverTaskStart());
+        generatingTask.setOnSucceeded( event -> OnSolverTaskEnd(generatingTask));
+
+        Thread generatorThread = new Thread(generatingTask);
+        generatorThread.start();
+    }
+
+    public void LoadGraph(){
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose Graph File");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        File file = fileChooser.showOpenDialog(new Popup());
+        if (file != null) {
+            try {
+                Graph graph = FileReaderG.readGraphFromFile(file);
+                Solver.getInstance().setGraph(graph);
+            } catch (IOException | FileReaderG.FileFormatError | Graph.InvalidMeshConnection e) {
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("File Read Error");
+                if(e instanceof FileReaderG.FileFormatError)
+                    errorAlert.setContentText(((FileReaderG.FileFormatError) e).getErrorMsg());
+                else
+                    errorAlert.setContentText(e.getMessage());
+                errorAlert.showAndWait();
+            }
+        }
+
+        Task<Graph> generatingTask = new Task<>() {
+            @Override
+            public Graph call() {
+                return null; //TODO
+            }
+        };
+        generatingTask.setOnRunning( event -> OnSolverTaskStart());
+        generatingTask.setOnSucceeded( event -> OnSolverTaskEnd(generatingTask));
+
+        Thread generatorThread = new Thread(generatingTask);
+        generatorThread.start();
+    }
+
+    public void SaveGraph(){
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose Graph File");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
+        File file = fileChooser.showSaveDialog(new Popup());
+        if (file != null) {
+            FileWriterG.writeGraphToFile(Solver.getInstance().getGraph(), file);
+        }
+
+        Task<Graph> generatingTask = new Task<>() {
+            @Override
+            public Graph call() {
+                return null; //TODO
             }
         };
         generatingTask.setOnRunning( event -> OnSolverTaskStart());
